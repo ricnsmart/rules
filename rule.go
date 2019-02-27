47ba0db62391ef38ebc3b83c70abf75d3f3a4ab2
voson wang <voson@ricnsmart.com>
2019-02-27 14:55:16 +0800

    feat(rule): 增加一个表示设备状态的常量

--- rule.go
+++ rule.go
@@ -2,11 +2,12 @@ package rules
 
 const (
 	// 设备状态
-	Inactive = 0 // 设备未激活
-	Online   = 1 // 设备正常在线
-	Offline  = 2 // 设备离线
-	Warn     = 3 // 设备预警
-	Alert    = 4 // 设备报警
+	Inactive     = 0 // 设备未激活
+	Online       = 1 // 设备正常在线
+	Offline      = 2 // 设备离线
+	Warn         = 3 // 设备预警
+	Alert        = 4 // 设备报警
+	Unregistered = 5 // 设备未注册
 
 	// Redis Key
 	DeviceStatus = `devices/status`
