17790d938d0c9ec2c74d81df544bc72e75c3ac64
voson wang <voson@ricnsmart.com>
2019-04-09 10:52:09 +0800

    refactor(rule): 移除设备的未注册状态

--- rule.go
+++ rule.go
@@ -9,12 +9,11 @@ const (
 	PMC350A = 5
 
 	// 设备状态
-	Inactive     = 0 // 设备未激活
-	Online       = 1 // 设备正常在线
-	Offline      = 2 // 设备离线
-	Warn         = 3 // 设备预警
-	Alert        = 4 // 设备报警
-	Unregistered = 5 // 设备未注册
+	Inactive = 0 // 设备未激活
+	Online   = 1 // 设备正常在线
+	Offline  = 2 // 设备离线
+	Warn     = 3 // 设备预警
+	Alert    = 4 // 设备报警
 
 	// Redis Key
 	DeviceStatus   = `devices/status`
